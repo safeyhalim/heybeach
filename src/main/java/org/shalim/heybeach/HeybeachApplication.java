ab059b7eeb67157ad787f485399a932e6e4abc85
Safey Abdelhalim <safey.abdelhalim@cognizant.com>
2018-11-12 21:09:40 +0100

    Initial commit

---
+++ src/main/java/org/shalim/heybeach/HeybeachApplication.java
@@ -0,0 +1,12 @@
+package org.shalim.heybeach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeybeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeybeachApplication.class, args);
+	}
+}
---
+++ src/test/java/org/shalim/heybeach/HeybeachApplicationTests.java
@@ -0,0 +1,16 @@
+package org.shalim.heybeach;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeybeachApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
